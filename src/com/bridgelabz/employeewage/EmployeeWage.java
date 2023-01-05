e89e60fefc5d999cb8a10daf444a4c82441b60c8
rajivjajo <rajivjajodia82552@gmail.com>
2023-01-05 18:31:19 +0530

    Daily working hours and per hour wages of employees

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -1,7 +1,19 @@
 package com.bridgelabz.employeewage;
 
 public class EmployeeWage {
+    static final int WAGE_PER_HOUR=20;
+    static final int FULL_DAY_HOUR=8;
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage");
+        int emplyeeChech=(int)(Math.random()*10)%2;
+        int dailyWage=0;
+        if (emplyeeChech==1){
+            dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+        System.out.println("Employee is present and his daily wage is : " +dailyWage);
+
+        }else {
+            System.out.println("Employee is absent");
+        }
+
     }
 }
