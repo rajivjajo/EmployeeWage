37f2ec26089a7e5db555719eb595ed4331efa831
rajivjajo <rajivjajodia82552@gmail.com>
2023-01-22 19:19:12 +0530

    Employee Wage computation for multiple companies

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -1,46 +1,42 @@
 package com.bridgelabz.employeewage;
 
 public class EmployeeWage {
-    static final int WAGE_PER_HOUR = 20;
-    static final int FULL_DAY_HOUR = 12;
+    public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
 
-    static final int PART_TIME_HOUR = 7;
+        static int computeEmpWage(String company , int empRatePerHr , int numOfWorkingDays , int maxHrPerMonth){
+            int empHrs = 0 , totalEmpHrs = 0 , totalWorkingDays = 0;
 
-    static final int WORKING_DAYS_PER_MONTH = 20;
-
-    static final int WORKING_HOURS_PER_MONTH = 100;
-        static void DaiLyWages(){
-            int dayCount = 1;
-            int workingHrs = 0;
-            while (dayCount <= WORKING_DAYS_PER_MONTH && workingHrs < WORKING_HOURS_PER_MONTH ) {
+            while (totalWorkingDays <= numOfWorkingDays && totalEmpHrs < maxHrPerMonth ) {
+                totalWorkingDays++;
                 int employeeCheck = (int) (Math.random() * 10) % 3;
                 System.out.println(employeeCheck);
-                int dailyWage = 0;
                 switch (employeeCheck) {
-                    case 1:
-                        System.out.println("Employee is Part Time");
-                        dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
-                        workingHrs = workingHrs + PART_TIME_HOUR;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
                         break;
-                    case 2:
-                        System.out.println("Employee is Full Time");
-                        dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-                        workingHrs = workingHrs + FULL_DAY_HOUR;
+                    case IS_PART_TIME:
+                        empHrs = 4;
                         break;
                     default:
-                        System.out.println("Employee is Absent");
+                        empHrs = 0;
                 }
-                System.out.println("Daily wage of Day => " + dayCount + " is "  + dailyWage);
-                System.out.println("working hrs => " + workingHrs);
-                dayCount++;
+                totalEmpHrs += empHrs;
+                System.out.println("Day# " + totalWorkingDays + " Employee Hours : " + empHrs);
             }
-        }
+            int totalEmpwage = empRatePerHr * totalEmpHrs;
+            System.out.println("Total employee wage for "+company+" is : $"+totalEmpwage);
+            return totalEmpwage;
+            }
+
 
 
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to Employee Wage");
-        DaiLyWages();
+        System.out.println(computeEmpWage("Amazon",1000,21,170));
+        System.out.println(computeEmpWage("Google",1199,20,165));
+
     }
 }
